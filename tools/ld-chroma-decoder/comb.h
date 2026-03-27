49b25e5357e19b22b7b4e023ae2bc0f9e2a2411d
Kraz3yIvan <quinnliam@hotmail.com>
2026-03-27 00:27:15 +0000

    Update comb.h

--- tools/ld-chroma-decoder/comb.h
+++ tools/ld-chroma-decoder/comb.h
@@ -10,19 +10,6 @@
 
     This file is part of ld-decode-tools.
 
-    ld-chroma-decoder is free software: you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 ************************************************************************/
 
 #ifndef COMB_H
@@ -32,19 +19,22 @@
 #include <QDebug>
 #include <QFile>
 #include <QtMath>
+#include <vector> 
 
 #include "lddecodemetadata.h"
 
 #include "componentframe.h"
 #include "decoder.h"
 #include "sourcefield.h"
+#include <fstream>
+#include <mutex>
+#include <cstdlib>
 
 class Comb
 {
 public:
     Comb();
 
-    // Comb filter configuration parameters
     struct Configuration {
         double chromaGain = 1.0;
         double chromaPhase = 0.0;
@@ -56,11 +46,6 @@ public:
         double cNRLevel = 0.0;
         double yNRLevel = 0.0;
 
-        // Adaptation sensitivity for 3D filter (higher = prefer 3D, lower = prefer 1D/2D)
-        // Default 1.0, range typically 0.5-2.0
-        double adaptThreshold = 1.0;
-        double chromaWeight = 1.0;
-
         qint32 getLookBehind() const;
         qint32 getLookAhead() const;
     };
@@ -69,41 +54,42 @@ public:
     void updateConfiguration(const LdDecodeMetaData::VideoParameters &videoParameters,
                              const Configuration &configuration);
 
-    // Decode a sequence of fields into a sequence of interlaced frames
     void decodeFrames(const QVector<SourceField> &inputFields, qint32 startIndex, qint32 endIndex,
                       QVector<ComponentFrame> &componentFrames);
 
-    // Maximum frame size
     static constexpr qint32 MAX_WIDTH = 910;
     static constexpr qint32 MAX_HEIGHT = 525;
 
-protected:
-
 private:
-    // Comb-filter configuration parameters
     bool configurationSet;
     Configuration configuration;
     LdDecodeMetaData::VideoParameters videoParameters;
 
-    // An input frame in the process of being decoded
     class FrameBuffer {
     public:
         FrameBuffer(const LdDecodeMetaData::VideoParameters &videoParameters_, const Configuration &configuration_);
 
         void loadFields(const SourceField &firstField, const SourceField &secondField);
 
+        // OLA Accumulators
+        std::vector<std::vector<double>> accChroma;
+        std::vector<std::vector<double>> weightSum;
+
         void split1D();
         void split2D();
-        void split3D(const FrameBuffer &previousFrame, const FrameBuffer &nextFrame);
+        
+        // [FIX] Adjusted for 4-Field Block (Current + Next)
+        void split3D(FrameBuffer &nextFrame,int frameIdx);
 
         void setComponentFrame(ComponentFrame &_componentFrame) {
             componentFrame = &_componentFrame;
         }
 
+        void finalizeOLA();
+
         void splitIQ();
         void splitIQlocked();
         void filterIQ();
-        void filterIQFull();
         void adjustY();
         void doCNR();
         void doYNR();
@@ -115,31 +101,21 @@ private:
         const LdDecodeMetaData::VideoParameters &videoParameters;
         const Configuration &configuration;
 
-        // Calculated frame height
         qint32 frameHeight;
-
-        // IRE scaling
         double irescale;
-
-        // Baseband samples (interlaced to form a complete frame)
         SourceVideo::Data rawbuffer;
-
-        // Chroma phase of the frame's two fields
         qint32 firstFieldPhaseID;
         qint32 secondFieldPhaseID;
 
-        // 1D, 2D and 3D-filtered chroma samples
         struct Sample {
             double pixel[MAX_HEIGHT][MAX_WIDTH];
         } clpbuffer[3];
 
-        // Result of evaluating a 3D candidate
         struct Candidate {
             double penalty;
             double sample;
         };
 
-        // The component frame for output (if there is one)
         ComponentFrame *componentFrame;
 
         inline qint32 getFieldID(qint32 lineNumber) const;
